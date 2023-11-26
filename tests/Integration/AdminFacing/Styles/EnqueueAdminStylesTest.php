da2b5cfa51716fec06170723d467a809f6957162
Bobby Lee <bobby@bobbylee.dev>
2023-11-26 13:02:46 -0800

    updates EnqueueAdminStylesTest

--- tests/Integration/AdminFacing/Styles/EnqueueAdminStylesTest.php
+++ tests/Integration/AdminFacing/Styles/EnqueueAdminStylesTest.php
@@ -27,16 +27,13 @@ beforeEach(
 	function () {
 		parent::setUp();
 
-		$this->admin_styles = new EnqueueAdminStyles();
-		//set_current_screen( 'toplevel_page_' . $this->plugin_slug() );
-		set_current_screen( 'dashboard' );
-
 		// Create a user and log them in
 		$this->user_id = $this::factory()->user->create();
 		wp_set_current_user( $this->user_id );
+		set_current_screen( 'dashboard' );
 
-		// Trigger the wp hook to set up user capabilities
-		do_action( 'wp' );
+		$this->admin_styles = new EnqueueAdminStyles();
+		$this->admin_styles->run();
 
 		// Create a test page
 		$this->test_page_id = $this::factory()->post->create( array(
@@ -49,7 +46,6 @@ beforeEach(
 		$this->styles  = $wp_styles;
 		$this->version = $this->plugin_version();
 	}
-
 );
 
 afterEach(
@@ -68,11 +64,11 @@ test(
 	'Admin Styles are not enqueued if not logged in.',
 	function () {
 		wp_set_current_user( 0 );
-		do_action( 'wp_enqueue_scripts', $this->admin_styles->enqueue_admin_styles() );
+		do_action( 'admin_enqueue_scripts' );
 		$this->go_to( get_permalink( $this->test_page_id ) );
-		expect( wp_style_is( $this->plugin_slug() . '-style', 'enqueued' ) )->toBeFalse();
+		expect( is_admin() )->toBeFalse()
+		                    ->and( wp_style_is( $this->plugin_slug() . '-style', 'enqueued' ) )->toBeFalse();
 	}
-
 );
 
 test(
@@ -80,42 +76,37 @@ test(
 	function () {
 		set_current_screen( 'admin' );
 		do_action( 'admin_enqueue_scripts' );
-		expect( wp_style_is( $this->plugin_slug() . '-style', 'enqueued' ) )->toBeTrue();
+		expect( is_admin() )->toBeTrue()
+		                    ->and( wp_style_is( $this->plugin_slug() . '-style', 'enqueued' ) )->toBeTrue();
 	}
-
 );
 
 test(
-	'Admin Styles are enqueued if on homepage while logged in.',
+	'Admin Styles are enqueued if on frontend while logged in.',
 	function () {
 		$this->go_to( get_permalink( $this->test_page_id ) );
-		echo get_the_title();
-		// Set up post data for the homepage
-		expect( wp_style_is( $this->plugin_slug() . '-style', 'enqueued' ) )->toBeTrue();
+		expect( is_admin() )->toBeFalse()
+		                    ->and( is_user_logged_in() )->toBeTrue()
+		                    ->and( wp_style_is( $this->plugin_slug() . '-style', 'enqueued' ) )->toBeTrue();
 	}
-
 );
 
 it( 'should have the styles enqueued on the plugin admin page.',
 	function () {
-		set_current_screen( 'toplevel_page_' . $this->plugin_slug() );
-		do_action( 'admin_enqueue_scripts', $this->admin_styles->enqueue_admin_styles() );
+		do_action( 'admin_enqueue_scripts' );
 		expect( is_admin() )->toBeTrue()
-		                    ->and( wp_style_is( $this->plugin_slug() . '-style', 'registered' ) )->toBeTrue()
-		                    ->and( is_home() )->toBeFalse();
+		                    ->and( wp_style_is( $this->plugin_slug() . '-style', 'registered' ) )->toBeTrue();
 	} );
 
 it( 'should have the plugin style handle/name',
 	function () {
-		set_current_screen( 'toplevel_page_' . $this->plugin_slug() );
-		do_action( 'admin_enqueue_scripts', $this->admin_styles->enqueue_admin_styles() );
+		do_action( 'admin_enqueue_scripts' );
 		expect( $this->styles->registered[ $this->plugin_slug() . '-style' ] )->toHaveProperty( 'handle', $this->plugin_slug() . '-style' );
 	} );
 
 it( 'should have the src which contains "/build/adminFacing/Main.css".',
 	function () {
-		set_current_screen( 'toplevel_page_' . $this->plugin_slug() );
-		do_action( 'admin_enqueue_scripts', $this->admin_styles->enqueue_admin_styles() );
+		do_action( 'admin_enqueue_scripts' );
 		expect( $this->styles->registered[ $this->plugin_slug() . '-style' ] )->toHaveProperty( 'src' );
 		assertTrue( str_contains(
 			$this->styles->registered[ $this->plugin_slug() . '-style' ]->src,
@@ -124,7 +115,6 @@ it( 'should have the src which contains "/build/adminFacing/Main.css".',
 
 it( 'should have the version that matches the plugin version.',
 	function () {
-		set_current_screen( 'toplevel_page_' . $this->plugin_slug() );
-		do_action( 'admin_enqueue_scripts', $this->admin_styles->enqueue_admin_styles() );
+		do_action( 'admin_enqueue_scripts' );
 		expect( $this->styles->registered[ $this->plugin_slug() . '-style' ]->ver )->toEqual( $this->version );
 	} );
